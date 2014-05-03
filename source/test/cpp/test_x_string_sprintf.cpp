1e5d5ee582ebf6db21cea7705d737089cdf2893e
jurgen <jurgen@virtuos.com.cn>
2014-05-03 13:36:14 +0800

    *: added 2 other unittests for sprintf

--- source/test/cpp/test_x_string_sprintf.cpp
+++ source/test/cpp/test_x_string_sprintf.cpp
@@ -1,4 +1,5 @@
 #include "xbase\x_string_ascii.h"
+#include "xbase\x_string_utf.h"
 
 #if defined(TARGET_PS3)
 	#include <math.h>
@@ -28,6 +29,26 @@ UNITTEST_SUITE_BEGIN(xsprintf)
 			CHECK_EQUAL(8, l);
 			CHECK_EQUAL(0, x_strCompare("3.141500", buffer));
 		}
+
+		UNITTEST_TEST(format_string)
+		{
+			char buffer[256];
+			const char* teststr = "test string";
+			s32 l = x_sprintf(buffer, sizeof(buffer) - 1, "the %s", x_va(teststr));
+			CHECK_EQUAL(15, l);
+			CHECK_EQUAL(0, x_strCompare("the test string", buffer));
+		}
+
+		UNITTEST_TEST(format_string8)
+		{
+			char buffer[256];
+			const char* teststr = "test string";
+			s32 l = utf::x_sprintf((ustr8*)buffer, sizeof(buffer) - 1, (const ustr8*)"the %s", x_va(teststr));
+			CHECK_EQUAL(15, l);
+			CHECK_EQUAL(0, x_strCompare("the test string", buffer));
+		}
+
+
 	}
 }
 UNITTEST_SUITE_END
