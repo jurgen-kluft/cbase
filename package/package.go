91696fc81a52233197aecf4421a9087a11b1f73c
Jurgen <jurgen@virtuos.com.cn>
2016-03-04 18:30:22 +0800

    setting up default cpp project has moved to denv

--- package/package.go
+++ package/package.go
@@ -1,14 +1,10 @@
 package xbase
 
-import (
-	"github.com/jurgen-kluft/xbase/package"
-	"github.com/jurgen-kluft/xcode/denv"
-)
+import "github.com/jurgen-kluft/xcode/denv"
 
 // GetProject returns the package object of 'xbase'
 func GetProject() *denv.Project {
-	xcode.SetupDefaultCppProject("xbase", "github.com\\jurgen-kluft")
-	project.Dependencies = append(project.Dependencies, xbase.GetProject())
+	denv.SetupDefaultCppProject("xbase", "github.com\\jurgen-kluft")
 
 	return project
 }
