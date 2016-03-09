d9be148e49ba631e778afd9ab9996ba2a187755b
Jurgen <jurgen@virtuos.com.cn>
2016-03-09 14:17:32 +0800

    update package

--- package/package.go
+++ package/package.go
@@ -1,10 +1,13 @@
 package xbase
 
-import "github.com/jurgen-kluft/xcode/denv"
+import (
+	"github.com/jurgen-kluft/xcode/denv"
+	"github.com/jurgen-kluft/xunittest/package"
+)
 
 // GetProject returns the package object of 'xbase'
 func GetProject() *denv.Project {
 	project := denv.SetupDefaultCppProject("xbase", "github.com\\jurgen-kluft")
-
+	project.Dependencies = append(project.Dependencies, xunittest.GetProject())
 	return project
 }
