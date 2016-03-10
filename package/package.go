e892ddcacdab2173563f5b1900249e0290350d21
Jurgen <jurgen@virtuos.com.cn>
2016-03-10 12:35:04 +0800

    *: update to use denv.Package

--- package/package.go
+++ package/package.go
@@ -5,9 +5,21 @@ import (
 	"github.com/jurgen-kluft/xunittest/package"
 )
 
-// GetProject returns the package object of 'xbase'
-func GetProject() *denv.Project {
-	project := denv.SetupDefaultCppProject("xbase", "github.com\\jurgen-kluft")
-	project.Dependencies = append(project.Dependencies, xunittest.GetProject())
-	return project
+// GetPackage returns the package object of 'xbase'
+func GetPackage() *denv.Package {
+	unittestpkg := xunittest.GetPackage()
+
+	// The main (xbase) package
+	mainpkg := denv.NewPackage("xbase")
+
+	// 'xbase' library
+	mainlib := denv.SetupDefaultCppLibProject("xbase", "github.com\\jurgen-kluft\\xbase")
+	mainlib.Dependencies = append(mainlib.Dependencies, unittestpkg.GetMainLib())
+
+	// 'xbase' unittest project
+	maintest := denv.SetupDefaultCppTestProject("xbase_test", "github.com\\jurgen-kluft\\xbase")
+
+	mainpkg.AddMainLib(mainlib)
+	mainpkg.AddUnittest(maintest)
+	return mainpkg
 }
