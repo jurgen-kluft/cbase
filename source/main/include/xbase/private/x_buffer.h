deeed0512c85e761bb5f7fc99a7219473cac11e3
Jurgen <jurgen@virtuos.com.cn>
2017-12-15 18:08:33 +0800

    * making xbuffer and xcbuffer, xchars and xchars first class citizens

--- source/main/include/xbase/private/x_buffer.h
+++ source/main/include/xbase/private/x_buffer.h
@@ -1,4 +1,197 @@
-inline void		xbuffer::reset()
+
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+inline				xchars::xchars(uchar* str)
+	: m_str(str)
+	, m_end(str)
+	, m_eos(str)
+{
+	while (*m_end != '\0')
+		++m_end;
+	m_eos = m_end;
+}
+inline				xchars::xchars(uchar* str, uchar* end)
+	: m_str(str)
+	, m_end(end)
+	, m_eos(end)
+{
+}
+
+inline				xchars::xchars(uchar* str, uchar* end, uchar* eos)
+	: m_str(str)
+	, m_end(end)
+	, m_eos(eos)
+{
+}
+
+inline				xchars::xchars(uchar* str, uchar* eos, const xchars& other)
+	: m_str(str)
+	, m_end(str)
+	, m_eos(eos)
+{
+	copy(other);
+}
+
+inline u32			xchars::size() const { return m_end - m_str; }
+
+inline void			xchars::reset()
+{
+	m_str[0] = '\0';
+	m_end = m_str;
+}
+
+inline void			xchars::clear()
+{
+	m_str[0] = '\0';
+	m_end = m_str;
+	uchar* str = m_str;
+	while (str < m_eos)
+		*str++ = '\0';
+}
+
+inline s32			xchars::copy(const xchars& _other)
+{
+	xcchars other(_other.m_str, _other.m_end);
+	return copy(other);
+}
+
+inline s32			xchars::copy(const xcchars& other)
+{
+	uchar const* src = other.m_str;
+	uchar * dst = m_str;
+
+	while (dst < m_eos && src <= other.m_end)
+	{
+		*dst++ = *src++;
+	}
+
+	m_end = dst;
+	if (dst < m_eos)
+		m_end--;
+
+	return m_end - m_str;
+}
+
+inline s32			xchars::compare(const xchars& _other) const
+{
+	xcchars other(_other.m_str, _other.m_end);
+	return compare(other);
+}
+
+inline s32			xchars::compare(const xcchars& other) const
+{
+	if (size() < other.size())
+		return -1;
+	if (size() > other.size())
+		return 1;
+	for (u32 i = 0; i < size(); ++i)
+	{
+		if (m_str[i] < other.m_str[i])
+			return -1;
+		else if (m_str[i] > other.m_str[i])
+			return 1;
+	}
+	return 0;
+}
+
+inline xchars&	xchars::operator = (const xchars& other)
+{
+	copy(other);
+	return *this;
+}
+
+inline xchars&	xchars::operator = (const xcchars& other)
+{
+	copy(other);
+	return *this;
+}
+
+inline bool		xchars::operator == (const xchars& other) const
+{
+	return compare(other) == 0;
+}
+
+inline bool		xchars::operator != (const xchars& other) const
+{
+	return compare(other) != 0;
+}
+
+inline bool		xchars::operator == (const xcchars& other) const
+{
+	return compare(other) == 0;
+}
+
+inline bool		xchars::operator != (const xcchars& other) const
+{
+	return compare(other) != 0;
+}
+
+inline uchar		xchars::operator [] (int i) const
+{
+	if (i >= 0 && i < size())
+		return m_str[i];
+	return '\0';
+}
+
+
+
+inline s32			xcchars::compare(const xchars& _other) const
+{
+	xcchars other(_other.m_str, _other.m_end);
+	return compare(other);
+}
+
+inline s32			xcchars::compare(const xcchars& other) const
+{
+	if (size() < other.size())
+		return -1;
+	if (size() > other.size())
+		return 1;
+	for (u32 i = 0; i < size(); ++i)
+	{
+		if (m_str[i] < other.m_str[i])
+			return -1;
+		else if (m_str[i] > other.m_str[i])
+			return 1;
+	}
+	return 0;
+}
+
+inline bool		xcchars::operator == (const xchars& other) const
+{
+	return compare(other) == 0;
+}
+
+inline bool		xcchars::operator != (const xchars& other) const
+{
+	return compare(other) != 0;
+}
+
+inline bool		xcchars::operator == (const xcchars& other) const
+{
+	return compare(other) == 0;
+}
+
+inline bool		xcchars::operator != (const xcchars& other) const
+{
+	return compare(other) != 0;
+}
+
+inline uchar		xcchars::operator [] (int i) const
+{
+	if (i >= 0 && i < size())
+		return m_str[i];
+	return '\0';
+}
+
+
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------
+
+
+inline void			xbuffer::reset()
 {
 	for (u32 i = 0; i < size(); ++i)
 		m_data[i] = 0;
@@ -21,6 +214,16 @@ inline s32			xbuffer::compare(const xbuffer& other) const
 }
 
 inline xbuffer&	xbuffer::operator = (const xbuffer& other)
+{
+	u32 i = 0;
+	for (; i < size() && i < other.size(); ++i)
+		m_data[i] = other.m_data[i];
+	for (; i < size(); ++i)
+		m_data[i] = 0;
+	return *this;
+}
+
+inline xbuffer&	xbuffer::operator = (const xcbuffer& other)
 {
 	u32 i = 0;
 	for (; i < size() && i < other.size(); ++i)
@@ -116,5 +319,4 @@ inline xbyte		xcbuffer::operator [] (int i) const
 	ASSERT(i >= 0 && i < (s32)size());
 	return m_data[i];
 }
-
 
--- source/main/include/xbase/x_buffer.h
+++ source/main/include/xbase/x_buffer.h
@@ -9,21 +9,110 @@
 
 namespace xcore
 {
+	class xcchars;
+	class xcbuffer;
 
+	// --------------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------------
+	class xchars
+	{
+	public:
+					xchars(uchar* str);
+					xchars(uchar* str, uchar* end);
+					xchars(uchar* str, uchar* end, uchar* eos);
+					xchars(uchar* str, uchar* eos, const xchars& other);
+
+		u32			size() const;
+		void		reset();
+		void		clear();
+
+		xbuffer		buffer() const { return xbuffer(size(), (xbyte*)m_str); }
+		xcbuffer	cbuffer() const { return xcbuffer(size(), (const xbyte*)m_str); }
+
+		s32			copy(const xchars& other);
+		s32			copy(const xcchars& other);
+
+		s32			compare(const xchars& other) const;
+		s32			compare(const xcchars& other) const;
+
+		xchars&		operator = (const xchars& other);
+		xchars&		operator = (const xcchars& other);
+
+		bool		operator == (const xchars& other) const;
+		bool		operator != (const xchars& other) const;
+		bool		operator == (const xcchars& other) const;
+		bool		operator != (const xcchars& other) const;
+
+		uchar		operator [] (int i) const;
+
+		uchar*		m_str;
+		uchar*		m_end;
+		uchar*		m_eos;
+	};
+
+	class xcchars
+	{
+	public:
+		inline		xcchars(uchar const* str) : m_str(str), m_end(str) { while (*m_end != '\0') ++m_end; }
+		inline		xcchars(uchar const* str, uchar const* end) : m_str(str), m_end(end) { }
+
+		u32			size() const { return m_end - m_str; }
+
+		xcbuffer	buffer() const { return xcbuffer(size(), (const xbyte*)m_str); }
+
+		s32			compare(const xchars& other) const;
+		s32			compare(const xcchars& other) const;
+
+		bool		operator == (const xchars& other) const;
+		bool		operator != (const xchars& other) const;
+
+		bool		operator == (const xcchars& other) const;
+		bool		operator != (const xcchars& other) const;
+
+		uchar		operator [] (int i) const;
+
+		uchar const*m_str;
+		uchar const*m_end;
+	};
+
+	// --------------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------------
 	class xbuffer
 	{
 	public:
 		inline		xbuffer(u32 len, xbyte* data) : m_len(len), m_data(data) { reset(); }
 
-		u32			size() const { return m_len; }
+		u32			size() const					{ return m_len; }
 		void		reset();
+		void		clear();
+
+		xbuffer		buffer() const					{ return buffer_at(0); }
+		xbuffer		buffer_at(u32 idx) const		{ ASSERT(idx < len); return xbuffer(size() - idx, (xbyte*)&m_data[idx]); }
+
+		xcbuffer	cbuffer() const					{ return cbuffer_at(0); }
+		xcbuffer	cbuffer_at(u32 idx) const		{ ASSERT(idx < len); return xcbuffer(size() - idx, (xbyte*)&m_data[idx]); }
+
+		void		write(const xbuffer& other)		{ write_at(0, other.cbuffer()); }
+		void		write(const xcbuffer& other)	{ write_at(0, other); }
+
+		void		write_at(u32 idx, const xcbuffer& other)
+		{
+			ASSERT(idx < len);
+			xbyte* dst = &m_data[idx];
+			u32 dst_size = m_len - idx;
+			for (u32 i = 0; i < other.size() && i < dst_size; i++)
+				dst[i] = other[i];
+		}
 
 		s32			compare(const xbuffer& other) const;
 
 		xbuffer&	operator = (const xbuffer& other);
+		xbuffer&	operator = (const xcbuffer& other);
+
 		bool		operator == (const xbuffer& other) const;
 		bool		operator != (const xbuffer& other) const;
 
+		xbyte&		operator [] (int i);
 		xbyte		operator [] (int i) const;
 
 		s32			m_len;
@@ -63,6 +152,9 @@ namespace xcore
 			for (u32 i = 0; i < size(); ++i)									\
 				m_data[i] = fill;												\
 		}																		\
+		void		clear() {													\
+			reset(0);															\
+		}																		\
 		s32			compare(const name& other) const {							\
 			for (u32 i = 0; i < size(); ++i) {									\
 				if (m_data[i] < other.m_data[i])								\
@@ -74,30 +166,22 @@ namespace xcore
 		}																		\
 		name&		operator = (const xbuffer& other) {							\
 			u32 i = 0;															\
-			for (; i < size() && i<other.size(); ++i)							\
+			for (; i < size() && i < other.size(); ++i)							\
 				m_data[i] = other.m_data[i];									\
 			for (; i < size(); ++i)												\
 				m_data[i] = 0;													\
 			return *this;														\
 		}																		\
 		name&		operator = (const name& other) {							\
-			for (u32 i = 0; i < size(); ++i)									\
+			for (u32 i=0; i < size(); ++i)										\
 				m_data[i] = other.m_data[i];									\
 			return *this;														\
 		}																		\
 		bool		operator == (const name& other) const {						\
-			for (u32 i = 0; i < size(); ++i) {									\
-				if (m_data[i] != other.m_data[i])								\
-					return false;												\
-			}																	\
-			return true;														\
+			return compare(other) == 0;											\
 		}																		\
 		bool		operator != (const name& other) const {						\
-			for (u32 i = 0; i < size(); ++i) {									\
-				if (m_data[i] != other.m_data[i])								\
-					return true;												\
-			}																	\
-			return false;														\
+			return compare(other) != 0;											\
 		}																		\
 		bool		operator > (const name& other) const {						\
 			return compare(other) == 1;											\
@@ -111,12 +195,116 @@ namespace xcore
 		bool		operator <= (const name& other) const {						\
 			return compare(other) <= 0;											\
 		}																		\
+		xbyte&		operator [] (int i) {										\
+			ASSERT(i >= 0 && i < (s32)size());									\
+			return m_data[i];													\
+		}																		\
 		xbyte		operator [] (int i) const {									\
 			ASSERT(i >= 0 && i < (s32)size());									\
 			return m_data[i];													\
 		}																		\
 		xbyte		m_data[len];												\
 	}
+
+	#define XCHARS(name, len)													\
+	class name																	\
+	{																			\
+	public:																		\
+		inline		name() { reset(); }											\
+		inline		name(name const& other) { reset(); *this = other; }			\
+		inline		name(xcchars const& other) { reset(); *this = other; }		\
+		u32			size() const { return m_end - m_str; }						\
+		xchars		chars() { return xchars(m_str, m_end, m_eos); }				\
+		xcchars		cchars() const { return xcchars(m_str, m_end); }			\
+		void		reset() {													\
+			m_end = m_str;														\
+			*m_end = '\0';														\
+			m_eos = &m_str[len - 1];											\
+			*m_eos = '\0';														\
+		}																		\
+		void		clear() {													\
+			reset();															\
+		}																		\
+		s32			compare(const xcchars& other) const {						\
+			if (size() < other.size())											\
+				return -1;														\
+			if (size() > other.size())											\
+				return 1;														\
+			uchar const * R = other.m_str;										\
+			uchar const * L = m_str;											\
+			while (L < m_end && R < other.m_end) {								\
+				if (*L < *R) return -1;											\
+				if (*L > *R) return 1;											\
+				L++;															\
+				R++;															\
+			}																	\
+			return 0;															\
+		}																		\
+		s32			compare(const name& other) const {							\
+			uchar const * R = other.m_str;										\
+			uchar const * L = m_str;											\
+			while (L < m_end && R < other.m_end) {								\
+				if (*L < *R) return -1;											\
+				if (*L > *R) return 1;											\
+				L++;															\
+				R++;															\
+			}																	\
+			return 0;															\
+		}																		\
+		name&		operator = (const xcchars& other) {							\
+			uchar const* src = other.m_str;										\
+			uchar * dst = m_str;												\
+			while (dst < m_eos && src <= other.m_end)							\
+				*dst++ = *src++;												\
+			m_end = dst;														\
+			if (dst < m_eos)													\
+				m_end--;														\
+			return *this;														\
+		}																		\
+		name&		operator = (const name& other) {							\
+			uchar const* src = other.m_str;										\
+			uchar * dst = m_str;												\
+			while (dst < m_eos && src <= other.m_end)							\
+				*dst++ = *src++;												\
+			m_end = dst;														\
+			if (dst < m_eos)													\
+				m_end--;														\
+			return *this;														\
+		}																		\
+		bool		operator == (const name& other) const {						\
+			return compare(other) == 0;											\
+		}																		\
+		bool		operator != (const name& other) const {						\
+			return compare(other) != 0;											\
+		}																		\
+		bool		operator > (const name& other) const {						\
+			return compare(other) == 1;											\
+		}																		\
+		bool		operator >= (const name& other) const {						\
+			return compare(other) >= 0;											\
+		}																		\
+		bool		operator < (const name& other) const {						\
+			return compare(other) == -1;										\
+		}																		\
+		bool		operator <= (const name& other) const {						\
+			return compare(other) <= 0;											\
+		}																		\
+		uchar&		operator [] (int i) {										\
+			ASSSERT(i < size());												\
+			if (i >= 0 && i < (s32)size())										\
+				return m_str[i];												\
+		}																		\
+		uchar		operator [] (int i) const {									\
+			if (i >= 0 && i < (s32)size())										\
+				return m_str[i];												\
+			return '\0';														\
+		}																		\
+		uchar		m_str[len];													\
+		uchar *		m_end;														\
+		uchar *		m_eos;														\
+	}
+
+
 
 	// Inline functions
 	#include "xbase/private/x_buffer.h"
@@ -128,6 +316,15 @@ namespace xcore
 	XBUFFER(xbuffer256, 256);
 	XBUFFER(xbuffer512, 512);
 	XBUFFER(xbuffer1024, 1024);
+
+	XCHARS(xchars16, 16);
+	XCHARS(xchars32, 32);
+	XCHARS(xchars64, 64);
+	XCHARS(xchars128, 128);
+	XCHARS(xchars256, 256);
+	XCHARS(xchars512, 512);
+	XCHARS(xchars1024, 1024);
+
 }
 
 #endif	///< __XCORE_BUFFER_H__
