cfe1f97d48ef14de3524c1602f66f467d8778c9d
Jurgen Kluft <jurgen@virtuosgames.com>
2019-01-16 17:57:38 +0800

    * xbtree with unittests

--- source/main/include/xbase/x_btree.h
+++ source/main/include/xbase/x_btree.h
@@ -34,28 +34,31 @@ namespace xcore
     xfsallocdexed* gCreateVMemBasedDexAllocator(xalloc* a, xvirtual_memory* vmem, u32 alloc_size, u64 addr_range, u32 page_size);
 
     //
-    // A btree_t is a BST that is unbalanced and where branches grow/shrink when adding
+    // A xbtree32 is a BST that is unbalanced and where branches grow/shrink when adding
     // or removing items. This particular implementation uses indices instead of pointers
     // and thus uses the same amount of memory on a 32-bit system compared to a 64-bit
     // system. It is possible to make an implementation where the indices are 16-bit when
-    // the number of leafs are less then 16384 (we already use the highest bit to identify
+    // the number of items are less then 16384 (we already use the highest bit to identify
     // between node and leaf plus a tree uses (N*2 - 1) nodes + leafs).
+    // But the node allocator need to be able to give back indices that are < 32768.
     //
     // We use an `indexer` here which is responsible for computing the index at every level
     // of the tree for a given 'key'. If the keydexer goes through the key from msb to lsb
     // then the keys are sorted in the btree, otherwise they are not sorted in the normal sense
     // but according to the output of the keydexer.
-    // The keydexer should be initialized in such a way so that the high frequency bits are
+    //
+    // The key_indexer should be initialized in such a way so that the high frequency bits are
     // taken first iterating over the bits from high-frequency to low-frequency.
+    //
     // In the case of the key being an index from 0-65536 and if you do not care about
-    // sorting you should initialize the keydexer like this:
-    // shift: 0, 2, 4, 6, 8, 10, 12, 14
-    // mask : 0x3
+    // sorting you should initialize the tree with init_from_index( _, _, max index).
+    //
     // The 'value' stored should only use bit 0-30, bit 31 is reserved.
     //
-    // Except the root every other node in this tree has 2 or more children.
+    // Adding two keys that are nearly identical results in the maximum branch length to be
+    // created, this is the worst case behaviour.
     //
-    // Since this data-structure is 'sorted' we also provide a lower- and upper-bound find
+    // Since this data-structure can be 'sorted' we also provide a lower- and upper-bound find
     // function.
     //
     struct xbtree32
@@ -69,8 +72,8 @@ namespace xcore
 
         void init(xfsallocdexed* node_allocator, keyvalue* kv);
         void init(xfsallocdexed* node_allocator, keyvalue* kv, key_indexer const* indexer);
-	    void init_from_index(xfsallocdexed* node_allocator, keyvalue* kv, u32 max_index);
-	    void init_from_mask(xfsallocdexed* node_allocator, keyvalue* kv, u64 mask, bool sorted);
+        void init_from_index(xfsallocdexed* node_allocator, keyvalue* kv, u32 max_index);
+        void init_from_mask(xfsallocdexed* node_allocator, keyvalue* kv, u64 mask, bool sorted);
 
         bool add(u64 key, u32 value);
         bool rem(u64 key, u32& value);
@@ -85,7 +88,7 @@ namespace xcore
         u32                m_root;
         key_indexer_data   m_idxr_data;
         key_indexer const* m_idxr;
-        xfsallocdexed*        m_node_alloc;
+        xfsallocdexed*     m_node_alloc;
         keyvalue*          m_kv;
     };
 
@@ -100,17 +103,19 @@ namespace xcore
 
         void init(xfsalloc* node_allocator, keyvalue* kv);
         void init(xfsalloc* node_allocator, keyvalue* kv, key_indexer const* idxr);
-	    void init_from_index(xfsalloc* node_allocator, keyvalue* kv, u32 max_index);
-	    void init_from_mask(xfsalloc* node_allocator, keyvalue* kv, u64 mask, bool sorted);
+        void init_from_index(xfsalloc* node_allocator, keyvalue* kv, u32 max_index);
+        void init_from_mask(xfsalloc* node_allocator, keyvalue* kv, u64 mask, bool sorted);
 
         bool add(u64 key, void* value);
         bool rem(u64 key, void*& value);
+        void clear();
 
         bool find(u64 key, void*& value) const;
         bool lower_bound(u64 key, void*& value) const;
         bool upper_bound(u64 key, void*& value) const;
 
         struct node_t;
+
     private:
         struct history_t;
         key_indexer const* m_idxr;
@@ -146,8 +151,8 @@ namespace xcore
             u64 m_key;
             T   m_value;
         };
-        xalloc*          m_allocator;
-        xbtree          m_tree;
+        xalloc* m_allocator;
+        xbtree  m_tree;
     };
 
     template <typename T> class xhashset
@@ -170,7 +175,7 @@ namespace xcore
         };
         hashfunc m_hash;
         xalloc*  m_allocator;
-        xbtree  m_tree;
+        xbtree   m_tree;
     };
 };
 
