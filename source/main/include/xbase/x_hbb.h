4287bd696fa439593fb09d0c2e835e8ef7240499
Jurgen <jurgen.kluft@gmail.com>
2022-01-10 21:13:17 +0800

    * hbb; rewrite to make it more reusable

--- source/main/include/xbase/x_hbb.h
+++ source/main/include/xbase/x_hbb.h
@@ -14,7 +14,7 @@ namespace xcore
     //  4/  128/   4KB/128KB/   4MB/128MB ( * ~1.5)
 
     // Example:
-    // When you initialize this bitlist with maxbits = 512Kbit the
+    // When you initialize a hbb_t with maxbits = 512Kbit the
     // size_in_dwords() function will return 16913, or 67652 bytes.
     // level 0, bits= 524288, dwords= 16384, bytes= 65536
     // level 1, bits= 16384, dwords= 512, bytes= 2048
@@ -22,41 +22,37 @@ namespace xcore
     // level 3, bits= 16, dwords= 1, bytes= 4
     // total: 65536 + 2048 + 64 + 4 = 67652 bytes
 
-    class hbb_t
+    struct hbb_t { u32* m_hbb; };
+    static u32 sizeof_hbb(u32 maxbits); // To know how many u32 to allocate
+
+    static void init(hbb_t hbb, u32 numbits);
+    static void init(hbb_t& hbb, u32 maxbits, alloc_t* alloc);
+    static void resize(hbb_t& hbb, u32 maxbits, alloc_t* alloc);
+    static void release(hbb_t hbb, alloc_t* alloc);
+    static void reset(hbb_t hbb);
+
+    static void set(hbb_t hbb, u32 bit);
+    static void clr(hbb_t hbb, u32 bit);
+
+    static bool is_set(hbb_t hbb, u32 bit);
+    static bool is_full(hbb_t hbb);
+
+    static bool find(hbb_t hbb, u32& bit);             // First 0 or 1
+    static bool upper(hbb_t hbb, u32 pivot, u32& bit); // First 0 or 1 equal to or greater than @pivot
+    static bool lower(hbb_t hbb, u32 pivot, u32& bit); // First 0 or 1 equal to or lesser than @pivot
+
+    enum
     {
-    public:
-        inline hbb_t() : m_numbits(0), m_maxlevel(0) {}
-
-        void init(u32* bits, u32 maxbits);
-        void init(alloc_t* alloc, u32 maxbits);
-        void resize(alloc_t* alloc, u32 maxbits);
-
-        void release(alloc_t* alloc);
-
-        void reset();
-
-        void set(u32 bit);
-        void clr(u32 bit);
-
-        bool is_set(u32 bit) const;
-        bool is_full() const;
-
-        bool find(u32& bit) const;             // First 0 or 1
-        bool upper(u32 pivot, u32& bit) const; // First 0 or 1 equal to or great than @pivot
-        bool lower(u32 pivot, u32& bit) const; // First 0 or 1 equal to or lesser than @pivot
-
-        static u32 size_in_dwords(u32 maxbits);
-
-        enum
-        {
-            AllBitsSet = 0xffffffff
-        };
-
-        // 7 levels maximum, this means a maximum of 7 * 5 = 2^35 = 34.359.738.368
-        u32* m_levels[7];
-        u32  m_numbits;
-        s32  m_maxlevel;
+        AllBitsSet = 0xffffffff
     };
+
+    //  3 bits; maximum number of levels = 7
+    // 29 bits; maximum number of bits 
+
+    // m_hbb[0] = num-levels(3 bits), num-bits(29 bits)
+    // m_hbb[1:1] = level 1 array, u32[1]
+    // m_hbb[2:34] = level 0 array, u32[32]        
+
 }; // namespace xcore
 
 #endif /// __X_HIERARCHICAL_BITSET_H__
