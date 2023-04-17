ec41efa941a92f3dd5833634894d9952dd60c405
Jurgen Kluft <jurgen@virtuosgames.com>
2023-04-17 10:32:17 +0800

    * unittest; test_alloc_t should take a ptr to TestAllocator pointer

--- source/test/include/cbase/test_allocator.h
+++ source/test/include/cbase/test_allocator.h
@@ -10,21 +10,20 @@
 
 class test_alloc_t : public ncore::alloc_t
 {
-    UnitTest::TestAllocator* mAllocator;
+    UnitTest::TestAllocator** mAllocator;
 
 public:
-    test_alloc_t(UnitTest::TestAllocator* allocator)
+    test_alloc_t(UnitTest::TestAllocator** allocator)
         : mAllocator(allocator)
     {
     }
-    virtual void*      v_allocate(ncore::u32 size, ncore::u32 align) { return mAllocator->Allocate(size, align); }
-    virtual ncore::u32 v_deallocate(void* p) { return mAllocator->Deallocate(p); }
+    virtual void*      v_allocate(ncore::u32 size, ncore::u32 align) { return (*mAllocator)->Allocate(size, align); }
+    virtual ncore::u32 v_deallocate(void* p) { return (*mAllocator)->Deallocate(p); }
     virtual void       v_release() {}
 };
 
-#define UNITTEST_ALLOCATOR \
-        static test_alloc_t TestAlloc(TestAllocator); \
-        static alloc_t* Allocator = &TestAlloc
-
+#define UNITTEST_ALLOCATOR                         \
+    static test_alloc_t TestAlloc(&TestAllocator); \
+    static alloc_t*     Allocator = &TestAlloc
 
 #endif // __TEST_ALLOCATOR_H__
