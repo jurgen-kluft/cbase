4fec14dbd24386ee549aa857f496f44bba1ac4e7
jurgen.kluft <jurgen.kluft@gmail.com>
2023-04-27 08:14:25 +0800

    * istream; more clearer (zero copy) functions

--- source/main/include/cbase/c_stream.h
+++ source/main/include/cbase/c_stream.h
@@ -13,7 +13,6 @@ namespace ncore
 		inline bool canSeek() const { return vcanSeek(); }
 		inline bool canRead() const { return vcanRead(); }
 		inline bool canWrite() const { return vcanWrite(); }
-		inline bool canZeroCopy() const { return vcanZeroCopy(); }	// Does this stream support zero-copy?
 
 		inline u64  getLength() const { return vgetLength(); }
 		inline void setLength(u64 length) { vsetLength(length); }
@@ -24,14 +23,16 @@ namespace ncore
 		inline void flush() { vflush(); }
 
 		inline s64 read(u8* data, s64 len) { return vread(data, len); }
-		inline s64 read0(u8 const*& data, s64 len) { return vread0(data, len); }
 		inline s64 write(u8 const* data, s64 len) { return vwrite(data, len); }
+
+		inline bool canView() const { return vcanView(); }	// Does this stream support zero-copy (can you use 'view()')
+		inline s64 view(u8 const*& data, s64 len) { return vview(data, len); }
 
 	protected:
 		virtual bool vcanSeek() const = 0;
 		virtual bool vcanRead() const = 0;
 		virtual bool vcanWrite() const = 0;
-		virtual bool vcanZeroCopy() const = 0;
+		virtual bool vcanView() const = 0;
 		virtual void vflush() = 0;
 		virtual void vclose() = 0;
 		virtual u64  vgetLength() const = 0;
@@ -39,7 +40,7 @@ namespace ncore
 		virtual s64  vsetPos(s64 pos) = 0;
 		virtual s64  vgetPos() const = 0;
 		virtual s64 vread(u8* buffer, s64 count) = 0;
-		virtual s64 vread0(u8 const*& buffer, s64 count) = 0;
+		virtual s64 vview(u8 const*& buffer, s64 count) = 0;
 		virtual s64 vwrite(const u8* buffer, s64 count) = 0;
 	};
 
