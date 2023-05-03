f2fc505dc91b90de21a2edd0a26adc302a77cbe1
jurgen.kluft <jurgen.kluft@gmail.com>
2023-05-03 09:56:41 +0800

    * dyntrie;  unittests for the utility functions

--- source/test/cpp/test_dyntrie.cpp
+++ source/test/cpp/test_dyntrie.cpp
@@ -1,24 +1,24 @@
 #define TEST_DYNTRIE
 #ifdef TEST_DYNTRIE
 
-#include "cbase/c_target.h"
-#include "cbase/c_allocator.h"
-#include "cbase/c_debug.h"
-#include "cbase/c_integer.h"
-#include "cbase/c_memory.h"
-#include "cbase/c_printf.h"
-#include "cbase/c_random.h"
-#include "cbase/c_runes.h"
+#    include "cbase/c_target.h"
+#    include "cbase/c_allocator.h"
+#    include "cbase/c_debug.h"
+#    include "cbase/c_integer.h"
+#    include "cbase/c_memory.h"
+#    include "cbase/c_printf.h"
+#    include "cbase/c_random.h"
+#    include "cbase/c_runes.h"
 
-#include "cunittest/cunittest.h"
-#include "cbase/test_allocator.h"
+#    include "cunittest/cunittest.h"
+#    include "cbase/test_allocator.h"
 
 namespace ncore
 {
     typedef const u8* dynkey_t;
     typedef const u8* dynval_t;
 
-    struct dynnode_t // 12 bytes
+    struct dynnode_t  // 12 bytes
     {
         inline bool is_value(s32 i) const { return (m_branch[i] & 0x80000000) == 0; }
         inline bool is_node(s32 i) const { return !is_value(i); }
@@ -42,52 +42,46 @@ namespace ncore
             m_bit_key = (m_bit_key & 0x00ffffff) | ((keylen & 0x000000ff) << 24);
         }
 
-        u32 m_branch[2]; // left/right (node or value (1 bit) : bit-index (7 bits) : 'node or value'-index (24 bits))
-        u32 m_bit_key;   // key-len (8 bits) : key-index (24 bits)
+        u32 m_branch[2];  // left/right (node or value (1 bit) : bit-index (7 bits) : 'node or value'-index (24 bits))
+        u32 m_bit_key;    // key-len (8 bits) : key-index (24 bits)
     };
+
+    static s32  get_bit(s8 keylen, dynkey_t key, s32 bit) { ASSERT((bit>>3)<keylen); return (key[(bit >> 3)] & (0x80 >> (bit & 0x07))) ? 1 : 0; }
+    static s32  ffs_bit(u8 byte);
+    static bool compare_keys(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start);
+    static s32  findfirst_bit_range(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start, s32 bit_end, dynkey_t nullkey);
+    static s32  findfirst_bit_range_fixedkey(s8 keylen, dynkey_t key1, dynkey_t key2, s32 bit_start, s32 bit_end);
 
     // Trie data structure where the key is a variable length byte array (strings, etc..)
     // No hashing
     // Lookup/Insert is O(Log2(N)) where N is the number of items in the trie, the key length is not so much
     // a factor in that the key range is visited in a range by range forward fashion.
-    // NOTE: We could also have a version where the keys are fixed length (byte sized) which would be quite faster.
+    // NOTE: We could also have a version where the keys are fixed length (byte sized) which would be faster.
     // NOTE: This node could even be 8 bytes if the total amount of items is less than 2^16.
     class dyntrie_t
     {
     public:
-        inline dyntrie_t()
-            : m_root(nullptr)
-            , m_key(nullptr)
-            , m_value(nullptr)
-            , m_count(0)
-            , m_capacity(1024)
-        {
-        }
+        inline dyntrie_t() : m_root(nullptr), m_key(nullptr), m_value(nullptr), m_count(0), m_capacity(1024) {}
 
         bool insert(s8 keylen, dynkey_t key, dynval_t value);
         bool find(s8 keylen, dynkey_t key, dynval_t& value) const;
         bool remove(s8 keylen, dynkey_t key, dynval_t& value);
 
     protected:
-        static s32  get_bit(s8 keylen, dynkey_t key, s32 bit) { return (key[(bit >> 3)] & (1 << (bit & 0x07))) != 0 ? 1 : 0; }
-        static s32  ffs_bit(u8 byte);
-        static bool compare_keys(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start);
-        static s32  findfirst_bit_range(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start, s32 bit_end, dynkey_t nullkey);
-
         static const char s_nullkey[256];
         dynnode_t*        m_root;
         dynkey_t          m_key;
         dynval_t          m_value;
         s32               m_count;
         s32               m_capacity;
-        dynnode_t         m_nodes[1024];  // the nodes
-        dynkey_t          m_keys[1024];   // pointers to keys, key/value are stored in pairs at the same index
-        dynval_t          m_values[1024]; // pointers to values
+        dynnode_t         m_nodes[1024];   // the nodes
+        dynkey_t          m_keys[1024];    // pointers to keys, key/value are stored in pairs at the same index
+        dynval_t          m_values[1024];  // pointers to values
     };
 
     const char dyntrie_t::s_nullkey[256] = {0};
 
-}; // namespace ncore
+};  // namespace ncore
 
 using namespace ncore;
 
@@ -96,11 +90,96 @@ UNITTEST_SUITE_BEGIN(test_dyntrie)
     UNITTEST_FIXTURE(main)
     {
         UNITTEST_ALLOCATOR;
+        u8 s_nullkey[256] = {0};
+
+        struct XorRandom
+        {
+            u64 s0, s1;
+            inline XorRandom(u64 seed) : s0(seed), s1(0)
+            {
+                next();
+                next();
+            }
+
+            inline u64 next(void)
+            {
+                u64 ss1    = s0;
+                u64 ss0    = s1;
+                u64 result = ss0 + ss1;
+                s0         = ss0;
+                ss1 ^= ss1 << 23;
+                s1 = ss1 ^ ss0 ^ (ss1 >> 18) ^ (ss0 >> 5);
+                return result;
+            }
+        };
+        static XorRandom s_rand(0x1234567890abcdef);
+
+        static void MakeKey(u8 * dst, u32 size, s32 bit)
+        {
+            for (u32 i = 0; i < size; ++i)
+            {
+                dst[i] = 0;
+            }
+            dst[bit >> 3] |= 0x80 >> (bit & 0x07);
+        }
 
         UNITTEST_FIXTURE_SETUP() {}
         UNITTEST_FIXTURE_TEARDOWN() {}
 
-        UNITTEST_TEST(trienode1) {}
+        UNITTEST_TEST(get_bit)
+        {
+            u8 key[20];
+
+            MakeKey(key, 20, 5);
+            CHECK_EQUAL(1, get_bit(20, key, 5));
+            CHECK_EQUAL(0, get_bit(20, key, 6));
+
+            MakeKey(key, 20, 85);
+            CHECK_EQUAL(1, get_bit(20, key, 85));
+            CHECK_EQUAL(0, get_bit(20, key, 86));
+        }
+
+        UNITTEST_TEST(ffs_bit)
+        {
+            CHECK_EQUAL(8, ffs_bit(0x00));
+            CHECK_EQUAL(7, ffs_bit(0x01));
+            CHECK_EQUAL(0, ffs_bit(0x80));
+        }
+
+        UNITTEST_TEST(compare_keys)
+        {
+            u8 key1[20];
+            u8 key2[20];
+            
+            MakeKey(key1, 20, 5);
+            MakeKey(key2, 20, 5);
+            CHECK_TRUE(compare_keys(20, key1, 20, key2, 0));
+
+            MakeKey(key1, 20, 5);
+            MakeKey(key2, 20, 6);
+            CHECK_FALSE(compare_keys(20, key1, 20, key2, 0));
+
+            MakeKey(key1, 20, 100);
+            MakeKey(key2, 20, 100);
+            CHECK_TRUE(compare_keys(20, key1, 20, key2, 30));
+        }
+
+        UNITTEST_TEST(findfirst_bit_range) 
+        {
+            u8 key1[20];
+            u8 key2[20];
+            
+            MakeKey(key1, 20, 10);
+            MakeKey(key2, 20, 32);
+
+            CHECK_EQUAL(10, findfirst_bit_range(20, key1, 20, key2, 0, 16, s_nullkey));
+            CHECK_EQUAL(10, findfirst_bit_range(20, key1, 20, key2, 0, 30, s_nullkey));
+            CHECK_EQUAL(10, findfirst_bit_range(20, key1, 20, key2, 10, 60, s_nullkey));
+
+            CHECK_EQUAL(32, findfirst_bit_range(20, key1, 20, key2, 11, 60, s_nullkey));
+        }
+
+
         UNITTEST_TEST(insert_find) {}
         UNITTEST_TEST(insert_find_1) {}
         UNITTEST_TEST(insert_find_2) {}
@@ -114,12 +193,12 @@ UNITTEST_SUITE_END
 
 #else
 
-#include "cbase\c_target.h"
-#include "cbase\c_allocator.h"
-#include "cbase\c_debug.h"
-#include "cbase\c_integer.h"
+#    include "cbase\c_target.h"
+#    include "cbase\c_allocator.h"
+#    include "cbase\c_debug.h"
+#    include "cbase\c_integer.h"
 
-#include "cunittest\cunittest.h"
+#    include "cunittest\cunittest.h"
 
 UNITTEST_SUITE_BEGIN(dyntrie_t)
 {
@@ -135,13 +214,13 @@ UNITTEST_SUITE_END
 
 namespace ncore
 {
-    s32 dyntrie_t::ffs_bit(u8 byte) { return math::findLastBit(byte); }
+    s32 ffs_bit(u8 byte) { return math::findLastBit(byte); }
 
     // nullkey is used to compare the key outside the range of a key
     // return:
     // - bit_end + 1 if the two keys are equal in the range
     // - bit_start + n if the two keys are different in the range and the n-th bit is different
-    s32 dyntrie_t::findfirst_bit_range(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start, s32 bit_end, dynkey_t nullkey)
+    s32 findfirst_bit_range(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start, s32 bit_end, dynkey_t nullkey)
     {
         // first we make sure that the key with the smaller length is key1
         if (keylen1 > keylen2)
@@ -151,7 +230,6 @@ namespace ncore
         }
 
         s32 byte_start = bit_start >> 3;
-        s32 byte_end   = bit_end >> 3;
         if (byte_start > keylen1)
         {
             key1 = nullkey;
@@ -164,6 +242,7 @@ namespace ncore
             return bit_end + 1;
 
         // start and end are in the same byte
+        const s32 byte_end = bit_end >> 3;
         if (byte_start == byte_end)
         {
             const u8 byte1 = key1[byte_start];
@@ -172,7 +251,7 @@ namespace ncore
             if ((byte1 & mask) == (byte2 & mask))
                 return bit_end + 1;
 
-            return bit_start + ffs_bit((byte1 ^ byte2) & mask);
+            return (byte_start << 3) + ffs_bit((byte1 ^ byte2) & mask);
         }
 
         // start and end are not in the same byte
@@ -182,7 +261,7 @@ namespace ncore
             const u8 mask  = 0xFF >> (bit_start & 7);
             if ((byte1 & mask) != (byte2 & mask))
             {
-                return bit_start + ffs_bit((byte1 ^ byte2) & mask);
+                return (byte_start << 3) + ffs_bit((byte1 ^ byte2) & mask);
             }
         }
 
@@ -233,7 +312,60 @@ namespace ncore
         return bit_end + 1;
     }
 
-    bool dyntrie_t::compare_keys(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start)
+    s32 findfirst_bit_range_fixedkey(s8 keylen, dynkey_t key1, dynkey_t key2, s32 bit_start, s32 bit_end)
+    {
+        s32 byte_start = bit_start >> 3;
+
+        // start and end are in the same byte
+        const s32 byte_end = bit_end >> 3;
+        if (byte_start == byte_end)
+        {
+            const u8 byte1 = key1[byte_start];
+            const u8 byte2 = key2[byte_start];
+            const u8 mask  = (0xFF >> (bit_start & 7)) & (0xFF << (7 - (bit_end & 7)));
+            if ((byte1 & mask) == (byte2 & mask))
+                return bit_end + 1;
+
+            return (byte_start << 3) + ffs_bit((byte1 ^ byte2) & mask);
+        }
+
+        // start and end are not in the same byte
+        {
+            const u8 byte1 = key1[byte_start];
+            const u8 byte2 = key2[byte_start];
+            const u8 mask  = 0xFF >> (bit_start & 7);
+            if ((byte1 & mask) != (byte2 & mask))
+            {
+                return (byte_start << 3) + ffs_bit((byte1 ^ byte2) & mask);
+            }
+        }
+
+        // see if there are bytes between start and end
+        // are we going outside any of the keys?
+        while (++byte_start < byte_end)
+        {
+            const u8 byte1 = key1[byte_start];
+            const u8 byte2 = key2[byte_start];
+            if (byte1 != byte2)
+            {
+                return (byte_start << 3) + ffs_bit(byte1 ^ byte2);
+            }
+        }
+
+        {
+            const u8 byte1 = key1[byte_end];
+            const u8 byte2 = key2[byte_end];
+            const u8 mask  = 0xFF << (7 - (bit_end & 7));
+            if ((byte1 & mask) != (byte2 & mask))
+            {
+                return (byte_end << 3) + ffs_bit((byte1 ^ byte2) & mask);
+            }
+        }
+
+        return bit_end + 1;
+    }
+
+    bool compare_keys(s8 keylen1, dynkey_t key1, s8 keylen2, dynkey_t key2, s32 bit_start)
     {
         if (keylen1 != keylen2)
             return false;
@@ -249,6 +381,11 @@ namespace ncore
             {
                 return false;
             }
+        }
+        else
+        {
+            if (key1[byte_start] != key2[byte_start])
+                return false;
         }
 
         // compare the rest byte by byte
@@ -289,18 +426,16 @@ namespace ncore
                         return false;
                     }
                     else
-                    { // keys are not the same so we need to insert a new node
+                    {  // keys are not the same so we need to insert a new node
 
                         // TODO: insert a new node
                         // create a new node
                         // set the new node's key to the key of the current node
-                        
-
 
                         return false;
                     }
                 }
-                else // branch is a node, so we need to continue the search
+                else  // branch is a node, so we need to continue the search
                 {
                     node      = &m_nodes[node->branch(branch)];
                     bit_start = bit_diff;
@@ -354,12 +489,12 @@ namespace ncore
                         return true;
                     }
                     else
-                    { // keys are not the same so we need to insert a new node
+                    {  // keys are not the same so we need to insert a new node
                         // TODO: insert a new node
                         return false;
                     }
                 }
-                else // branch is a node, so we need to continue the search
+                else  // branch is a node, so we need to continue the search
                 {
                     const s32 branch = node->branch(branch);
                     if (branch == 0)
@@ -378,4 +513,4 @@ namespace ncore
 
     bool dyntrie_t::remove(s8 keylen, dynkey_t key, dynval_t& value) { return false; }
 
-} // namespace ncore
+}  // namespace ncore
