a0a1ba2c2a4859932dbe2bd06294a61ea9591a6d
jurgen.kluft <jurgen.kluft@gmail.com>
2024-05-08 07:59:00 +0800

    * cbase; mac system allocator, use posix_memalign

--- source/main/cpp/c_allocator_system_mac.cpp
+++ source/main/cpp/c_allocator_system_mac.cpp
@@ -11,46 +11,47 @@
 
 namespace ncore
 {
-    void* mac_aligned_malloc(size_t size, size_t alignment)
-    {
-        if (alignment < 2*(sizeof(void*)))
-        {
-            alignment = 2*(sizeof(void*));
-        }
-        if (size < (sizeof(void*)))
-        {
-            size = (sizeof(void*));
-        }
+    // void* mac_aligned_malloc(size_t size, size_t alignment)
+    // {
+    //     if (alignment < 2*(sizeof(void*)))
+    //     {
+    //         alignment = 2*(sizeof(void*));
+    //     }
+    //     if (size < (sizeof(void*)))
+    //     {
+    //         size = (sizeof(void*));
+    //     }
 
-        void* p1; // original block
-        if ((p1 = malloc(size + 2*alignment)) == nullptr)
-            return nullptr;
 
-        void** p2; // aligned block
-        p2     = (void**)(((ptr_t)(p1) & ~(alignment - 1)));
-        p2     = (void**)(((ptr_t)(p2) + 2*alignment));
-        p2[-2] = (void*)size;
-        p2[-1] = p1;
-        return (void*)p2;
-    }
+    //     void* p1; // original block
+    //     if ((p1 = malloc(size + 2*alignment)) == nullptr)
+    //         return nullptr;
 
-    void mac_aligned_free(void* ptr) { free(((void**)ptr)[-1]); }
+    //     void** p2; // aligned block
+    //     p2     = (void**)(((ptr_t)(p1) & ~(alignment - 1)));
+    //     p2     = (void**)(((ptr_t)(p2) + 2*alignment));
+    //     p2[-2] = (void*)size;
+    //     p2[-1] = p1;
+    //     return (void*)p2;
+    // }
 
-    void* mac_aligned_realloc(void* ptr, size_t size, size_t alignment)
-    {
-        size_t old_size = (size_t)((void**)ptr)[-2];
-        void*  new_ptr  = mac_aligned_malloc(size, alignment);
-        memcpy(new_ptr, ptr, math::min(size, old_size));
-        mac_aligned_free(ptr);
-        return new_ptr;
-    }
+    // void mac_aligned_free(void* ptr) { free(((void**)ptr)[-1]); }
+
+    // void* mac_aligned_realloc(void* ptr, size_t size, size_t alignment)
+    // {
+    //     size_t old_size = (size_t)((void**)ptr)[-2];
+    //     void*  new_ptr  = mac_aligned_malloc(size, alignment);
+    //     memcpy(new_ptr, ptr, math::min(size, old_size));
+    //     mac_aligned_free(ptr);
+    //     return new_ptr;
+    // }
 
     class allocator_macos_system : public alloc_t
     {
     public:
         allocator_macos_system()
             : mInitialized(0)
-            , mDefaultAlignment(4)
+            , mDefaultAlignment(sizeof(void*))
             , mAllocationCount(0)
         {
         }
@@ -59,14 +60,14 @@ namespace ncore
         {
             mInitialized      = 1;
             mAllocationCount  = 0;
-            mDefaultAlignment = 4;
+            mDefaultAlignment = sizeof(void*);
         }
 
         void exit()
         {
             ASSERTS(mAllocationCount == 0, "ERROR: System Allocator is being released but still has allocations that are not freed");
             mInitialized      = 0;
-            mDefaultAlignment = 0;
+            mDefaultAlignment = sizeof(void*);
             mAllocationCount  = 0;
         }
 
@@ -74,7 +75,10 @@ namespace ncore
 
         virtual void* v_allocate(u32 size, u32 alignment)
         {
-            void* ptr = mac_aligned_malloc(size, alignment);
+            alignment = math::alignUp(alignment, mDefaultAlignment);
+
+            void* ptr;
+            posix_memalign(&ptr, alignment, size);
             ++mAllocationCount;
             return ptr;
         }
@@ -82,11 +86,11 @@ namespace ncore
         virtual void v_deallocate(void* ptr)
         {
             --mAllocationCount;
-            mac_aligned_free(ptr);
+            free(ptr);
         }
 
         s32 mInitialized;
-        s32 mDefaultAlignment;
+        u32 mDefaultAlignment;
         u64 mAllocationCount;
     };
 
