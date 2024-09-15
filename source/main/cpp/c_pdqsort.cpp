e13d252af63d533eafe203419d43d94574941259
jurgen.kluft <jurgen.kluft@gmail.com>
2024-09-15 17:03:36 +0800

    * cbase
    * reformat comments in pdq sort

--- source/main/cpp/c_pdqsort.cpp
+++ source/main/cpp/c_pdqsort.cpp
@@ -2,31 +2,31 @@
 
 namespace ncore
 {
-    /*****************************************************************************
-     * pdqsort.c -- A public domain implementation of an iterative version of *
-     * the QuickSort algorithm with "Median of Three" pivot selection, *
-     * "end recursion removal", and the use of an Insertion Sort for small *
-     * partitions. Many people have "improved" C. A. R. Hoare's original *
-     * QuickSort algorithm. This implementation follows Robert Sedgewick's *
-     * ("Algorithms in C", Robert Sedgewick, Addison-Wesley, 1990, *
-     * ISBN 0-201-51425-7) improvements. *
-     * *
-     * An interative qsort() is faster and requires less stack space than the *
-     * recursive implementation. If "end recursion removal" is employed, the *
-     * maximum number of stack entries required will be no more than log to the *
-     * base 2 of the number of elements in the array being sorted. *
-     * *
-     * Sedgewick states that the use of an Insertion Sort for partitions smaller *
-     * than M elements, where M is a constant between 5 and 25, results in *
-     * about a 20% improvement in the efficiency of the sort. *
-     *****************************************************************************/
+    // *****************************************************************************
+    // * pdqsort.c -- A public domain implementation of an iterative version of    *
+    // * the QuickSort algorithm with "Median of Three" pivot selection,           *
+    // * "end recursion removal", and the use of an Insertion Sort for small       *
+    // * partitions. Many people have "improved" C. A. R. Hoare's original         *
+    // * QuickSort algorithm. This implementation follows Robert Sedgewick's       *
+    // * ("Algorithms in C", Robert Sedgewick, Addison-Wesley, 1990,               *
+    // * ISBN 0-201-51425-7) improvements.                                         *
+    // *                                                                           *
+    // * An interative qsort() is faster and requires less stack space than the    *
+    // * recursive implementation. If "end recursion removal" is employed, the     *
+    // * maximum number of stack entries required will be no more than log to the  *
+    // * base 2 of the number of elements in the array being sorted.               *
+    // *                                                                           *
+    // * Sedgewick states that the use of an Insertion Sort for partitions smaller *
+    // * than M elements, where M is a constant between 5 and 25, results in       *
+    // * about a 20% improvement in the efficiency of the sort.                    *
+    // *****************************************************************************
 
-    /****************************************************************************
-     * Swap() -- The routine used by the main qsort() routine to swap elements *
-     * of the array when necessary. To save time, Swap() will swap in integer *
-     * units until there is less than a full integer left. Then it will swap *
-     * in byte units until the entire element has been swapped. *
-     ****************************************************************************/
+    //  ****************************************************************************
+    //  * Swap() -- The routine used by the main qsort() routine to swap elements  *
+    //  * of the array when necessary. To save time, Swap() will swap in integer   *
+    //  * units until there is less than a full integer left. Then it will swap    *
+    //  * in byte units until the entire element has been swapped.                 *
+    //  ****************************************************************************
 
     static void Swap(void *a, void *b, u32 intCount, u32 byteCount)
     {
@@ -50,44 +50,44 @@ namespace ncore
             ((u8 *&)b)++;
         }
     }
-    
-    const int M = 8; /* The threshold for Insertion */
 
-    /*****************************************************************************
-     * The main qsort() routine. This implementation is fully compatible with *
-     * the standard qsort() routine in the ANSI C run time libraries. *
-     * *
-     * base is the base address of the array to be sorted, nelem is the number of*
-     * entries in the array to be sorted, width is the width of a single element *
-     * in bytes, and fcmp is a pointer to a function that performs the comparison*
-     * between elements of the array. *
-     * *
-     * The pointer L scans partitions from the Left, the pointer R scans *
-     * partitions from the right, Limit is a pointer that serves as a sentinel *
-     * to stop the scan on the right (the pivot element is stored at base and *
-     * serves as the left end sentinel). InsertThresh is the M constant *
-     * converted to a pointer for comparisons. *
-     *****************************************************************************/
+    const int M = 8; // The threshold for Insertion
+
+    //  *****************************************************************************
+    //  * The main qsort() routine. This implementation is fully compatible with    *
+    //  * the standard qsort() routine in the ANSI C run time libraries.            *
+    //  *                                                                           *
+    //  * base is the base address of the array to be sorted, nelem is the number of*
+    //  * entries in the array to be sorted, width is the width of a single element *
+    //  * in bytes, and fcmp is a pointer to a function that performs the comparison*
+    //  * between elements of the array.                                            *
+    //  *                                                                           *
+    //  * The pointer L scans partitions from the Left, the pointer R scans         *
+    //  * partitions from the right, Limit is a pointer that serves as a sentinel   *
+    //  * to stop the scan on the right (the pivot element is stored at base and    *
+    //  * serves as the left end sentinel). InsertThresh is the M constant          *
+    //  * converted to a pointer for comparisons.                                   *
+    //  *****************************************************************************
     void qsort(void *_base, u32 nelem, u32 width, int (*fcmp)(const void *a, const void *b))
     {
         char    *base = (char *)_base;
         char    *L, *R, *Limit;
         unsigned InsertThresh;
 
-        /***************************************************************************
-         * The stack used to push the bounds of the larger partition. Thirty-two *
-         * entries will allow for sorting an array with a little over 4 million *
-         * entries! *
-         ***************************************************************************/
+        //  ***************************************************************************
+        //  * The stack used to push the bounds of the larger partition. Thirty-two   *
+        //  * entries will allow for sorting an array with a little over 4 million    *
+        //  * entries!                                                                *
+        //  ***************************************************************************
         struct
         {
             char *Base;
             char *Limit;
         } Stack[32], *Sptr;
 
-        /* Initialization */
-        const u32 IntCount  = width / sizeof(int);
-        const u32 ByteCount = width % sizeof(int);
+        // Initialization
+        const u32 IntCount  = width >> 2;
+        const u32 ByteCount = width & 3;
 
         InsertThresh = M * width;
         Sptr         = Stack;
@@ -100,11 +100,11 @@ namespace ncore
                 L = (char *)base + width;
                 R = Limit - width;
 
-                /**************************************************************************
-                 * The following code implements Sedgewick's "Median of Three" selection *
-                 * for the pivot element. The central element is selected as the first *
-                 * tentative pivot *
-                 **************************************************************************/
+                //  *************************************************************************
+                //  * The following code implements Sedgewick's "Median of Three" selection *
+                //  * for the pivot element. The central element is selected as the first   *
+                //  * tentative pivot                                                       *
+                //  *************************************************************************
 
                 Swap(((unsigned)(Limit - base) >> 1) - ((((unsigned)(Limit - (char *)base) >> 1)) % width) + (char *)base, base, IntCount, ByteCount);
                 if ((*fcmp)(L, R) > 0)
@@ -114,9 +114,9 @@ namespace ncore
                 if ((*fcmp)(L, base) > 0)
                     Swap(L, base, IntCount, ByteCount);
 
-                /*************************************************
-                 * The following code performs the partitioning *
-                 *************************************************/
+                //  *************************************************
+                //  * The following code performs the partitioning  *
+                //  *************************************************
 
                 while (1)
                 {
@@ -128,11 +128,11 @@ namespace ncore
                 }
                 Swap(base, R, IntCount, ByteCount);
 
-                /**************************************************************************
-                 * The following code performs the "end recursion removal". This is *
-                 * accomplished by stacking the larger partition and immediately sorting *
-                 * the smaller. *
-                 **************************************************************************/
+                // **************************************************************************
+                // * The following code performs the "end recursion removal". This is       *
+                // * accomplished by stacking the larger partition and immediately sorting  *
+                // * the smaller.                                                           *
+                // **************************************************************************
 
                 if (R - base > Limit - L)
                 {
@@ -149,10 +149,10 @@ namespace ncore
                 Sptr++;
             }
 
-            /*****************************************************************************
-             * The following code is the Insertion Sort used to sort partitions smaller *
-             * the M elements. *
-             *****************************************************************************/
+            // *****************************************************************************
+            // * The following code is the Insertion Sort used to sort partitions smaller  *
+            // * the M elements.                                                           *
+            // *****************************************************************************
 
             L = (char *)base + width;
             while (L < Limit)
@@ -166,10 +166,10 @@ namespace ncore
                 L += width;
             }
 
-            /****************************************************************************
-             * If there are any partitions left on the stack, pop one off and sort it. *
-             * Otherwise, the qsort is finished. *
-             ****************************************************************************/
+            // ****************************************************************************
+            // * If there are any partitions left on the stack, pop one off and sort it.  *
+            // * Otherwise, the qsort is finished.                                        *
+            // ****************************************************************************
             if (Sptr <= Stack)
                 break;
 
