ef27c5872cf7d100589d6e25f581c30fd72894a4
jurgen.kluft <jurgen.kluft@gmail.com>
2024-10-13 15:07:26 +0800

    * cbase
    * binmap unittest, added 2 tests from csuperalloc
    * update test main

--- source/test/cpp/test_hbb.cpp
+++ source/test/cpp/test_hbb.cpp
@@ -198,6 +198,105 @@ UNITTEST_SUITE_BEGIN(test_hbb_t)
 
             hbb.release(Allocator);
         }
+
+        UNITTEST_TEST(set_get)
+        {
+            u32       l0len, l1len, l2len, l3len;
+            u32 const len = 32 * 32 * 32 * 32;
+            binmap_t::compute_levels(len, l0len, l1len, l2len, l3len);
+
+            u32* l1 = (u32*)Allocator->allocate(sizeof(u32) * ((l1len + 31) >> 5));
+            u32* l2 = (u32*)Allocator->allocate(sizeof(u32) * ((l2len + 31) >> 5));
+            u32* l3 = (u32*)Allocator->allocate(sizeof(u32) * ((l3len + 31) >> 5));
+            nmem::memclr(l1, sizeof(u32) * ((l1len + 31) >> 5));
+            nmem::memclr(l2, sizeof(u32) * ((l2len + 31) >> 5));
+            nmem::memclr(l3, sizeof(u32) * ((l3len + 31) >> 5));
+
+            for (s32 i = 0; i < 64; ++i)
+            {
+                u32 const count = i * 1024 + ((1 - (i & 1)) * 600);
+                binmap_t::compute_levels(count, l0len, l1len, l2len, l3len);
+                binmap_t bm;
+                bm.init_all_free(count, l0len, l1, l1len, l2, l2len, l3, l3len);
+
+                for (u32 b = 0; b < count; b += 2)
+                {
+                    bm.set_used(b);
+                }
+                for (u32 b = 0; b < count; b++)
+                {
+                    bool const s = bm.get(b);
+                    CHECK_EQUAL(((b & 1) == 0), s);
+                }
+                for (u32 b = 1; b < count; b += 2)
+                {
+                    u32 const f = bm.find_and_set();
+                    CHECK_EQUAL(b, f);
+                }
+
+                // There should not be any more free places in the binmap
+                s32 const f = bm.find();
+                CHECK_EQUAL(-1, f);
+            }
+
+            Allocator->deallocate(l1);
+            Allocator->deallocate(l2);
+            Allocator->deallocate(l3);
+        }
+
+        UNITTEST_TEST(lazy_init)
+        {
+            u32       l0len, l1len, l2len, l3len;
+            u32 const len = 32 * 32 * 32 * 32;
+            binmap_t::compute_levels(len, l0len, l1len, l2len, l3len);
+
+            u32* l1 = (u32*)Allocator->allocate(sizeof(u32) * ((l1len + 31) >> 5));
+            u32* l2 = (u32*)Allocator->allocate(sizeof(u32) * ((l2len + 31) >> 5));
+            u32* l3 = (u32*)Allocator->allocate(sizeof(u32) * ((l3len + 31) >> 5));
+            nmem::memclr(l1, sizeof(u32) * ((l1len + 31) >> 5));
+            nmem::memclr(l2, sizeof(u32) * ((l2len + 31) >> 5));
+            nmem::memclr(l3, sizeof(u32) * ((l3len + 31) >> 5));
+
+            for (u32 i = 0; i < 64; ++i)
+            {
+                u32 const count = i * 1024 + ((1 - (i & 1)) * 600);
+                binmap_t::compute_levels(count, l0len, l1len, l2len, l3len);
+
+                binmap_t bm;
+                bm.init_all_free_lazy(count, l0len, l1, l1len, l2, l2len, l3, l3len);
+
+                for (u32 b = 0; b < count; ++b)
+                {
+                    bm.tick_all_free_lazy(b);
+                }
+
+                for (u32 b = 0; b < count; b += 2)
+                {
+                    CHECK_FALSE(bm.get(b));
+                    bm.set_used(b);
+                    CHECK_TRUE(bm.get(b));
+                }
+                for (u32 b = 0; b < count; b++)
+                {
+                    bool const s = bm.get(b);
+                    CHECK_EQUAL(((b & 1) == 0), s);
+                }
+
+                for (u32 b = 1; b < count; b += 2)
+                {
+                    u32 const f = bm.find_and_set();
+                    CHECK_EQUAL(b, f);
+                }
+
+                // There should not be any more free places in the binmap
+                s32 const f = bm.find();
+                CHECK_EQUAL(-1, f);
+            }
+
+            Allocator->deallocate(l1);
+            Allocator->deallocate(l2);
+            Allocator->deallocate(l3);
+        }
     }
 }
 UNITTEST_SUITE_END
