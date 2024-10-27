5f4b238210111ede51f7498ef437715ea0ba921a
jurgen.kluft <jurgen.kluft@gmail.com>
2024-10-27 18:58:04 +0800

    * cbase
    * range_t and range16_t

--- source/test/cpp/test_range.cpp
+++ source/test/cpp/test_range.cpp
@@ -39,16 +39,18 @@ namespace ncore
 
         // node state is 1 bit per node, 0 = free, 0x80 = allocated
         // the lower 7 bits is the size range of the node
-        u8     m_size[256];            // Size is the size_index + 1, so 1 = 32 MB, 2 = 256 MB, 3 = 128 MB, etc.
-        node_t m_node_next[256];       // Next node in the size list
-        node_t m_node_prev[256];       // Previous node in the size list
-        u16    m_node_free[16];        // Single bit per node, 0 = free, 1 = allocated
-        node_t m_size_lists[16];       // Every size has its own list
-        u16    m_size_list_occupancy;  // Which size list index is occupied
-        u8     m_base_size_min;        // ilog2(address_range / 64)
-        u8     m_base_size_max;
+        u8*     m_node_size;            // Size is the size_index + 1, so 1 = 32 MB, 2 = 256 MB, 3 = 128 MB, etc.
+        node_t* m_node_next;            // Next node in the size list
+        node_t* m_node_prev;            // Previous node in the size list
+        u16*    m_node_free;            // Single bit per node, 0 = free, 1 = allocated
+        node_t  m_size_lists[16];       // Every size has its own list
+        u16     m_size_list_occupancy;  // Which size list index is occupied
+        u8      m_base_size_min;        // ilog2(address_range / 64)
+        u8      m_base_size_range;
+        u8      m_node_count_shift;
 
-        void setup(u64 address_range);
+        void setup(alloc_t* allocator, u64 address_range = 1 * cTB, u8 node_count_shift = 8);
+        void teardown(alloc_t* allocator);
 
         bool allocate(u64 size, u64& out_address);  // Returns false if the size is not available
         bool deallocate(u64 address);               // Deallocates the memory at the given address
@@ -59,41 +61,47 @@ namespace ncore
         void remove_size(u8 size_index, node_t node);
     };
 
-    void range_t::setup(u64 address_range)
+    void range_t::setup(alloc_t* allocator, u64 address_range, u8 node_count_shift)
     {
-        for (u32 i = 0; i < 16; ++i)
-        {
-            m_node_free[i]  = 0;
+        m_node_count_shift = node_count_shift;
+        m_base_size_min    = math::ilog2(address_range >> node_count_shift);
+        for (u32 i = 0; i < g_array_size(m_size_lists); ++i)
             m_size_lists[i] = c_null;
-        }
 
-        for (u32 i = 0; i < 256; ++i)
-        {
-            m_size[i]      = c_null;
-            m_node_next[i] = c_null;
-            m_node_prev[i] = c_null;
-        }
+        u32 const node_count = 1 << node_count_shift;
+        m_node_size          = g_allocate_array_and_clear<u8>(allocator, node_count);
+        m_node_next          = g_allocate_array_and_memset<node_t>(allocator, node_count, c_null);
+        m_node_prev          = g_allocate_array_and_memset<node_t>(allocator, node_count, c_null);
+        m_node_free          = g_allocate_array_and_clear<u16>(allocator, (node_count + 15) >> (sizeof(u16) * 2));
 
-        m_base_size_min               = math::ilog2(address_range / 256);
-        m_base_size_max               = math::ilog2(address_range) - m_base_size_min;
-        m_size_list_occupancy         = 1 << 8;   // Mark the largest size as occupied
-        m_size_lists[m_base_size_max] = 0;        // The largest node in the size list
-        m_size[0]                     = 256 - 1;  // Full size range
-        m_node_next[0]                = 0;        // Link the node to itself
-        m_node_prev[0]                = 0;        // Link the node to itself
+        m_base_size_min                 = math::ilog2(address_range / node_count);
+        m_base_size_range               = math::ilog2(address_range) - m_base_size_min;
+        m_size_list_occupancy           = 1 << m_base_size_range;  // Mark the largest size as occupied
+        m_size_lists[m_base_size_range] = 0;                       // The largest node in the size list
+        m_node_size[0]                  = node_count - 1;          // Full size range
+        m_node_next[0]                  = 0;                       // Link the node to itself
+        m_node_prev[0]                  = 0;                       // Link the node to itself
+    }
+
+    void range_t::teardown(alloc_t* allocator)
+    {
+        allocator->deallocate(m_node_size);
+        allocator->deallocate(m_node_next);
+        allocator->deallocate(m_node_prev);
+        allocator->deallocate(m_node_free);
     }
 
     bool range_t::split(node_t node)
     {
-        const span_t span  = (span_t)m_size[node] + 1;
+        const span_t span  = (span_t)m_node_size[node] + 1;
         const u8     index = math::ilog2(span);
 
         remove_size(index, node);
 
         // Split the node into two nodes
         node_t const insert = node + (span >> 1);
-        m_size[node]        = (node_t)((span >> 1) - 1);
-        m_size[insert]      = (node_t)((span >> 1) - 1);
+        m_node_size[node]   = (node_t)((span >> 1) - 1);
+        m_node_size[insert] = (node_t)((span >> 1) - 1);
 
         // Invalidate the next and previous pointers of the new node (debugging)
         m_node_next[insert] = c_null;
@@ -123,6 +131,7 @@ namespace ncore
         m_node_prev[head] = node;
         m_node_next[node] = head;
         m_node_prev[node] = prev;
+
         m_size_list_occupancy |= (1 << index);
     }
 
@@ -181,14 +190,14 @@ namespace ncore
         node_t node = (node_t)(address >> m_base_size_min);
         m_node_free[node >> 4] &= ~(1 << (node & 15));
 
-        span_t span  = (span_t)m_size[node] + 1;
+        span_t span  = (span_t)m_node_size[node] + 1;
         u8     index = math::ilog2(span);
 
         while (true)
         {
             // Could be that we are freeing the largest size allocation, this means
             // that we should not merge the node with its sibling.
-            if (span == 256)
+            if (index == m_node_count_shift)
                 break;
 
             const node_t sibling = ((node & ((span << 1) - 1)) == 0) ? node + span : node - span;
@@ -200,11 +209,11 @@ namespace ncore
             remove_size(index, sibling);                            // Sibling is being merged, remove it
             node_t const merged = node < sibling ? node : sibling;  // Always take the left node as the merged node
             node_t const other  = node < sibling ? sibling : node;  // Always take the right node as the other node
-            m_size[merged]      = (u8)((1 << (index + 1)) - 1);     // Double the span (size) of the node
-            m_size[other]       = c_null;                           // Invalidate the node that is merged into 'merged'
+            m_node_size[merged] = (u8)((1 << (index + 1)) - 1);     // Double the span (size) of the node
+            m_node_size[other]  = c_null;                           // Invalidate the node that is merged into 'merged'
 
             node  = merged;
-            span  = (span_t)m_size[node] + 1;
+            span  = (span_t)m_node_size[node] + 1;
             index = math::ilog2(span);
         }
 
@@ -228,7 +237,7 @@ UNITTEST_SUITE_BEGIN(range)
         UNITTEST_TEST(allocate)
         {
             range_t range;
-            range.setup(4 * cGB);
+            range.setup(Allocator, 4 * cGB, 8);
 
             u64 address = 0;
             CHECK_TRUE(range.allocate(32 * cMB, address));
@@ -251,12 +260,14 @@ UNITTEST_SUITE_BEGIN(range)
 
             CHECK_TRUE(range.allocate(1 * cGB, address));
             CHECK_EQUAL(1 * cGB, address);
+
+            range.teardown(Allocator);
         }
 
         UNITTEST_TEST(allocate_and_deallocate)
         {
             range_t range;
-            range.setup(4 * cGB);
+            range.setup(Allocator, 4 * cGB, 8);
 
             for (s32 i = 0; i < 16; ++i)
             {
@@ -276,6 +287,8 @@ UNITTEST_SUITE_BEGIN(range)
                 CHECK_TRUE(range.deallocate(address2));
                 CHECK_TRUE(range.deallocate(address3));
             }
+
+            range.teardown(Allocator);
         }
     }
 }
